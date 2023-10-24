7265883b7e94cc98b41bf04c385b40deb4e5c11a
davidamorim13 <146035206+davidamorim13@users.noreply.github.com>
2023-10-24 19:09:00 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+// const nome = "João"
+// let idade = 58
+// var marca = "volvo"
+
+// function somar( numero1, numero2){
+
+//     return numero1 + numero2
+// }
+// const total = somar(1,6)
+// console.log(total)
+
+const produtos = [
+     "arroz",
+     "margarina",
+     "feijao",
+]
+function mostraNome(produtos) {
+    console.log(produtos[1])
+}
+mostraNome(produtos)
