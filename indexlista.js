f320c76fb192c301b7502a9875c642d6e426fbf2
davidamorim13 <146035206+davidamorim13@users.noreply.github.com>
2023-10-30 16:10:28 -0300

    Add files via upload

---
+++ indexlista.js
@@ -0,0 +1,24 @@
+const  nomes  =  [
+    "David",
+    "Luiz",
+    "João",
+    "André"
+]
+
+const carros = [
+    "Onix",
+    "Ford KA",
+    "F-150",
+    "Mustang",
+
+]
+function  mostraNome ( nomes )  {
+   console . registro ( nomes [ 2 ] )
+   console . registro ( nomes [ 3 ] )
+}
+mostraNome ( nomes )
+function  mostraNome ( carros )  {
+    console . registro ( carros [ 2 ] )
+    console . registro ( carros [ 3 ] )
+ }
+ mostraNome ( carros )
